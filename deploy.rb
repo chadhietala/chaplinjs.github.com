0fc04c0fc9d1d488114aed463138043a6f36e9a6
Karel Ledru-Mathe <karel@ledrumathe.com>
2012-05-19 16:47:20 -0400

    Fix the output of the deploy script

--- deploy.rb
+++ deploy.rb
@@ -11,17 +11,14 @@ puts 'Remove old generated files ================='
  ! -name "README.md" \
  -delete`
 
-puts '\n\n'
-puts 'Copy new generated files ==================='
+puts "\nCopy new generated files ==================="
 `cp -rf jekyll/_site/* ./`
 
-puts '\n\n'
-puts 'Commit the files to git ===================='
+puts "\nCommit the files to git ===================="
 `git add .`
 `git reset -- jekyll/*`
 `git reset -- deploy.rb`
 `git commit -m "Update Jekyll generated HTML"`
 
-puts '\n\n'
-puts 'Deploy to github ==========================='
+puts "\nDeploy to github ==========================="
 `git push origin master`
